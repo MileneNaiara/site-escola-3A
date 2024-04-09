fbc175920077a65477dfaa8f49c6d58ac8398007
Milene Naiara Rebinski <108832930+MileneNaiara@users.noreply.github.com>
2024-04-09 11:45:18 -0300

    09-04

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ for (let i=0; i < botoes.length; i++){
 for (let j=0; j<botoes.length; j++){
     botoes[j].classList.remove("ativo");
     textos[j].classList.remove("ativo");
-}
-botoes [i].classList.add("ativo");
-textos [i].classList.add("ativo");
+    }
+    botoes [i].classList.add("ativo");
+    textos [i].classList.add("ativo");
     }
 }
 const tempoObjetivo1 = new Date("2024-03-04T00:00:00");
